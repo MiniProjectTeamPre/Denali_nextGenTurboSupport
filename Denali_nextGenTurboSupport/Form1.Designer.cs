33a97803d2ea26e056b61569d4511f1663b396b1
AnochaPay <Anocha_ping@hotmail.com>
2024-05-30 16:31:57 +0700

    add

---
+++ Denali_nextGenTurboSupport/Form1.Designer.cs
@@ -0,0 +1,383 @@
+﻿namespace Denali_nextGenTurboSupport {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
+            this.tb_sn1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.tb_result4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_sn4 = new System.Windows.Forms.TextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.tb_result3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tb_sn3 = new System.Windows.Forms.TextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tb_result2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tb_sn2 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tb_result1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.groupBox4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_sn1
+            // 
+            this.tb_sn1.Location = new System.Drawing.Point(294, 19);
+            this.tb_sn1.Name = "tb_sn1";
+            this.tb_sn1.Size = new System.Drawing.Size(229, 20);
+            this.tb_sn1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.checkBox4);
+            this.groupBox1.Controls.Add(this.checkBox3);
+            this.groupBox1.Controls.Add(this.checkBox2);
+            this.groupBox1.Controls.Add(this.checkBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.groupBox5);
+            this.groupBox1.Controls.Add(this.groupBox4);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1338, 718);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(488, 651);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(277, 61);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.pictureBox4);
+            this.groupBox5.Controls.Add(this.tb_result4);
+            this.groupBox5.Controls.Add(this.label4);
+            this.groupBox5.Controls.Add(this.tb_sn4);
+            this.groupBox5.Location = new System.Drawing.Point(6, 193);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(1326, 52);
+            this.groupBox5.TabIndex = 5;
+            this.groupBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox4.Location = new System.Drawing.Point(19, 17);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // tb_result4
+            // 
+            this.tb_result4.Location = new System.Drawing.Point(846, 19);
+            this.tb_result4.Name = "tb_result4";
+            this.tb_result4.Size = new System.Drawing.Size(130, 20);
+            this.tb_result4.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(63, 22);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(208, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "MEFKNVJLIEFUYJ2B3HBHJBD23BHRB2";
+            // 
+            // tb_sn4
+            // 
+            this.tb_sn4.Location = new System.Drawing.Point(294, 19);
+            this.tb_sn4.Name = "tb_sn4";
+            this.tb_sn4.Size = new System.Drawing.Size(229, 20);
+            this.tb_sn4.TabIndex = 0;
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.pictureBox3);
+            this.groupBox4.Controls.Add(this.tb_result3);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.tb_sn3);
+            this.groupBox4.Location = new System.Drawing.Point(6, 135);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(1326, 52);
+            this.groupBox4.TabIndex = 5;
+            this.groupBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox3.Location = new System.Drawing.Point(19, 17);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // tb_result3
+            // 
+            this.tb_result3.Location = new System.Drawing.Point(846, 19);
+            this.tb_result3.Name = "tb_result3";
+            this.tb_result3.Size = new System.Drawing.Size(130, 20);
+            this.tb_result3.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(63, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(208, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "MEFKNVJLIEFUYJ2B3HBHJBD23BHRB2";
+            // 
+            // tb_sn3
+            // 
+            this.tb_sn3.Location = new System.Drawing.Point(294, 19);
+            this.tb_sn3.Name = "tb_sn3";
+            this.tb_sn3.Size = new System.Drawing.Size(229, 20);
+            this.tb_sn3.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.pictureBox2);
+            this.groupBox3.Controls.Add(this.tb_result2);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.tb_sn2);
+            this.groupBox3.Location = new System.Drawing.Point(6, 77);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1326, 52);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox2.Location = new System.Drawing.Point(19, 17);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // tb_result2
+            // 
+            this.tb_result2.Location = new System.Drawing.Point(846, 19);
+            this.tb_result2.Name = "tb_result2";
+            this.tb_result2.Size = new System.Drawing.Size(130, 20);
+            this.tb_result2.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(63, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(208, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "MEFKNVJLIEFUYJ2B3HBHJBD23BHRB2";
+            // 
+            // tb_sn2
+            // 
+            this.tb_sn2.Location = new System.Drawing.Point(294, 19);
+            this.tb_sn2.Name = "tb_sn2";
+            this.tb_sn2.Size = new System.Drawing.Size(229, 20);
+            this.tb_sn2.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.pictureBox1);
+            this.groupBox2.Controls.Add(this.tb_result1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.tb_sn1);
+            this.groupBox2.Location = new System.Drawing.Point(6, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1326, 52);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Lime;
+            this.pictureBox1.Location = new System.Drawing.Point(19, 17);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(24, 24);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // tb_result1
+            // 
+            this.tb_result1.Location = new System.Drawing.Point(846, 19);
+            this.tb_result1.Name = "tb_result1";
+            this.tb_result1.Size = new System.Drawing.Size(130, 20);
+            this.tb_result1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "MEFKNVJLIEFUYJ2B3HBHJBD23BHRB2";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(25, 355);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(80, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.Click += new System.EventHandler(this.checkBox1_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(25, 378);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.Click += new System.EventHandler(this.checkBox2_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(25, 401);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(80, 17);
+            this.checkBox3.TabIndex = 9;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.Click += new System.EventHandler(this.checkBox3_Click);
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Location = new System.Drawing.Point(25, 424);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(80, 17);
+            this.checkBox4.TabIndex = 10;
+            this.checkBox4.Text = "checkBox4";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            this.checkBox4.Click += new System.EventHandler(this.checkBox4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1362, 742);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_sn1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.TextBox tb_result4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_sn4;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.TextBox tb_result3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tb_sn3;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox tb_result2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tb_sn2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tb_result1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Denali_nextGenTurboSupport/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Denali_nextGenTurboSupport {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+        
+        }
+        private void button1_Click(object sender, EventArgs e) {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            timer1.Enabled = false;
+
+            TextBoxTest();
+            DelaymS(3000);
+            TextBoxPass();
+
+            ClearSN();
+
+            timer1.Enabled = false;
+        }
+        public static void DelaymS(int mS) {
+            Stopwatch stopwatchDelaymS = new Stopwatch();
+            stopwatchDelaymS.Restart();
+            while (mS > stopwatchDelaymS.ElapsedMilliseconds) {
+                if (!stopwatchDelaymS.IsRunning) stopwatchDelaymS.Start();
+                Application.DoEvents();
+            }
+            stopwatchDelaymS.Stop();
+        }
+        private void TextBoxTest() {
+            string testing = "Testing";
+
+            if (pictureBox1.BackColor == Color.Lime) {
+                tb_result1.Text = testing;
+            }
+            if (pictureBox2.BackColor == Color.Lime) {
+                tb_result2.Text = testing;
+            }
+            if (pictureBox3.BackColor == Color.Lime) {
+                tb_result3.Text = testing;
+            }
+            if (pictureBox4.BackColor == Color.Lime) {
+                tb_result4.Text = testing;
+            }
+        }
+        private void TextBoxPass() {
+            string testing = "Pass";
+
+            if (pictureBox1.BackColor == Color.Lime) {
+                tb_result1.Text = testing;
+                GenLogTest("KF39A00V44-864593055057078-Denali Alkaline-Configure-Passed.xml");
+            }
+            if (pictureBox2.BackColor == Color.Lime) {
+                tb_result2.Text = testing;
+                GenLogTest("KF39A00V94-864593054946073-Denali Alkaline-Configure-Passed.xml");
+            }
+            if (pictureBox3.BackColor == Color.Lime) {
+                tb_result3.Text = testing;
+                GenLogTest("KF39A00VA4-864593055034655-Denali Alkaline-Configure-Passed.xml");
+            }
+            if (pictureBox4.BackColor == Color.Lime) {
+                tb_result4.Text = testing;
+                GenLogTest("KF39A00VB4-864593055034382-Denali Alkaline-Configure-Passed.xml");
+            }
+        }
+        private void ClearSN() {
+            tb_sn1.Text = string.Empty;
+            tb_sn2.Text = string.Empty;
+            tb_sn3.Text = string.Empty;
+            tb_sn4.Text = string.Empty;
+        }
+
+        private void GenLogTest(string nameFile) {
+            string data = File.ReadAllText("../../../" + nameFile);
+
+            string[] dataSup = data.Replace("<ReportTime>", "ฅ").Replace("</ReportTime>", "ฅ").Split('ฅ');
+            string time = dataSup[1];
+
+            string newTime = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss", 
+                System.Globalization.CultureInfo.CreateSpecificCulture("en-US"));
+
+            data = data.Replace(time, newTime);
+
+            while (true)
+            {
+                try
+                {
+                    File.WriteAllText("../../../Data Log Test/" + nameFile, data);
+                    break;
+                }
+                catch { }
+                Thread.Sleep(50);
+            }
+        }
+
+
+
+
+
+        private void checkBox1_Click(object sender, EventArgs e) {
+            if (checkBox1.Checked) {
+                pictureBox1.BackColor = Color.Lime;
+                label1.Visible = true;
+            } else {
+                pictureBox1.BackColor = Color.Red;
+                label1.Visible = false;
+            }
+        }
+        private void checkBox2_Click(object sender, EventArgs e) {
+            if (checkBox2.Checked) {
+                pictureBox2.BackColor = Color.Lime;
+                label2.Visible = true;
+            } else {
+                pictureBox2.BackColor = Color.Red;
+                label2.Visible = false;
+            }
+        }
+        private void checkBox3_Click(object sender, EventArgs e) {
+            if (checkBox3.Checked) {
+                pictureBox3.BackColor = Color.Lime;
+                label3.Visible = true;
+            } else {
+                pictureBox3.BackColor = Color.Red;
+                label3.Visible = false;
+            }
+        }
+        private void checkBox4_Click(object sender, EventArgs e) {
+            if (checkBox4.Checked) {
+                pictureBox4.BackColor = Color.Lime;
+                label4.Visible = true;
+            } else {
+                pictureBox4.BackColor = Color.Red;
+                label4.Visible = false;
+            }
+        }
+    }
+}
